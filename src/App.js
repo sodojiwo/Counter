79b37e534409b649f33694b9c753b3b07150b265
Arif Widianto <arifjozz123@gmail.com>
2020-10-19 11:02:20 +0700

    desctructure used state object

--- src/App.js
+++ src/App.js
@@ -6,20 +6,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <CounterCard>
-          <Header judul="Counter One" isi="This is the first counter!" />
-          <Counter />        
-        </CounterCard>
-      
-        <CounterCard>
-          <Header judul="Counter Two" isi="This is the second counter!" />
-          <Counter />        
-        </CounterCard>
-      
-        <CounterCard>
-          <Header judul="Counter Three" isi="This is the third counter!" />
-          <Counter />        
-        </CounterCard>
+        <Header judul="Halo" isi="tahu" />
+        <Counter />
+        <Header judul="Hai" isi="tahu2" />
+        <Counter />
+        <Header judul="Hola" isi="tahu3" />
+        <Counter />
       </div>
     );
   }
@@ -31,18 +23,8 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <h3>{this.props.judul}</h3>
+        <h1>{this.props.judul}</h1>
         <p>{this.props.isi}</p>
-      </div>
-    );
-  }
-}
-
-class CounterCard extends Component {
-  render() {
-    return (
-      <div style={{padding:"20px", background:"white", boxShadow:"0px 0px 15px rgba(0, 125, 197, 0.06)", borderRadius:"10px" }}>
-      {this.props.children}
       </div>
     );
   }
@@ -50,29 +32,37 @@ class CounterCard extends Component {
 
 class Counter extends Component {
   state = {
-    number: 0
+    angka: 0
   };
 
-  increment = () => {
+  tambah = () => {
+    const { angka } = this.state
+
     this.setState({
-      number: this.state.number + 1
+      angka: angka + 1
     });
   };
 
-  decrement = () => {
-    if (this.state.number == 0) {
+  minus = () => {
+    const { angka } = this.state
+    
+    if (angka < 1) {
       return alert('Jangan Minus woy!');
     }
+    
     this.setState({
-      number: this.state.angka - 1
+      angka: angka - 1
     });
   };
+
   render() {
+    const { angka } = this.state
+
     return (
       <div>
-        <h2 style={{}}>{this.state.number}</h2>
-        <button onClick={this.increment}>Incremen</button>
-        <button onClick={this.decrement}>Decrement</button>
+        <h2 style={{}}>{angka}</h2>
+        <button onClick={this.tambah}>Tambah</button>
+        <button onClick={this.minus}>Kurang</button>
       </div>
     );
   }
